5751ccfa8a96e0362c42c7f195a591d4eed455ac
Michael Wayne Goodman <goodman.m.w@gmail.com>
2015-12-14 18:27:25 -0800

    Very basic browser working with Xigt visualization.

--- valhalla.py
+++ valhalla.py
@@ -1,14 +1,19 @@
 
+import json
+
 from flask import Flask, render_template
 
 import sleipnir
 
 app = Flask(__name__)
-app.register_blueprint(sleipnir.blueprint)
+app.register_blueprint(sleipnir.v1)
+
+with open('config.json') as f:
+    settings = json.loads(f.read())
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', settings=settings)
 
 if __name__ == '__main__':
     app.run()
